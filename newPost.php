62ceb3e05940507523c8248adeade1130120adac
Mari Rogne <marirosta@hotmail.com>
2020-04-22 14:06:49 +0200

    test

--- newPost.php
+++ newPost.php
@@ -8,12 +8,12 @@ include('classes/jwt_token.php');
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-
+echo "Test";
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     
-    
+    echo "Test";
     
     // du trenger denne for å snakke med databasen. benytt så enten eksisterende metoder eller skriv
     // en ny
