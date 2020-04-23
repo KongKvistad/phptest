6f8a1abb9d1c9a601eb130cf64608f6a7805ff3a
Mari Rogne <marirosta@hotmail.com>
2020-04-23 16:15:46 +0200

    newPost.php

--- newPost.php
+++ newPost.php
@@ -1,9 +1,9 @@
 <?php
 
-include_once('classes/get_class.php');
+/* include_once('classes/get_class.php');
 
 include('classes/jwt_token.php');
-
+ */
 
 //header("Access-Control-Allow-Origin: *");
 //header("Content-Type: application/json; charset=UTF-8");
@@ -13,17 +13,20 @@ echo "Ny testt <br><br>";
 
 echo <<< _END
 
-    <form method='post' action='newPost.php' enctype='multipart/form-data'>
+    <form method='post' action='' enctype='multipart/form-data'>
         <label for="company"> Company name: </label>
         <input type ="text" id="company" name="company"><br>
         <label for ="title"> Title: </label>
         <input type ="text" id="title" name="title"><br>
         <label for ="author"> Author: </label>
         <input type="text" id="author" name="author"><br>
+
         <label for ="start-date">Start date: </label>
         <input type ="date" id="start-date" name="start-date"><br>
         <label for ="end-date">End date: </label>
         <input type ="date" id ="end-date" name="end-date"><br><br>
+
+
         <label for="description">Description: </label>
         <textarea id="description" name="description">
         </textarea><br><br>
@@ -35,9 +38,9 @@ echo <<< _END
                 </datalist> <br><br>
         
 
-        <input type ="radio" id="internship" name="internship" value="Interneship">        
+        <input type ="radio" id="internship" name="radiobutton" value="Internship">        
         <label for ="internship"> Internship</label><br>
-        <input type="radio" id="bachelorproject" name="bachelorproject" value="Bachelor Project">
+        <input type="radio" id="bachelorproject" name="radiobutton" value="Bachelorproject">
         <label for="bachelorproject">Bachelor project</label><br><br>
         
 
@@ -45,7 +48,7 @@ echo <<< _END
         <label for="tags">Tags: </label>
         <input type="text" id="tags" name="tags"><br>
         <label for ="iamstudent">I am student: </label>
-        <input type="checkbox" id="iamstudent" name="iamstudent" value="I am student"><br> <br>
+        <input type="checkbox" id="iamstudent" name="checkbox" value="I am student"><br> <br>
         
         <input type='submit' value = 'Submit'>
     </form>
@@ -54,26 +57,71 @@ _END;
 
 //print_r($_POST);
 
+
+
+
+
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    
-    
-    // du trenger denne for å snakke med databasen. benytt så enten eksisterende metoder eller skriv
-    // en ny
-    $connect = new Connection;
+
+    $connect = mysqli_connect('localhost', 'root', '');
+        
+       
+    mysqli_select_db($connect, 'webprosjekt2');
 
     $company = $_POST['company'];
-    $title = $_POST['title'];
-    $author = $_POST['author'];
-    $startDate = $_POST['start-date'];
-    $endDate = $_POST['end-date'];
-    $description = $_POST['description'];
-    $noOfstudents = $_POST['noofstudents'];
+        $title = $_POST['title'];
+        $author = $_POST['author'];
+        $startDate = $_POST['start-date'];
+        $endDate = $_POST['end-date'];
+        $description = $_POST['description'];
+        $noOfstudents = $_POST['noofstudents'];
+        $tags = $_POST['tags'];
 
-    $query = "INSERT INTO internship(`companyName`, `title`, `author`, `startDate`, `endDate`, `description`, `noOfstudents`) VALUES('$company', '$title', '$author', $startDate, $endDate, '$description', '$noOfstudents')";
-    $connect->postData($query);
-    
-    //$connect->close();
+        $starttime = date('Y-m-d', strtotime($startDate));
+        $endtime = date('Y-m-d', strtotime($endDate));
+
+
+
+    if($_POST['radiobutton'] == "Internship") {
+        echo "internship";
+        
+        
+        if($_POST['checkbox'] == "I am student"){
+        
+            $query = "INSERT INTO internship(`companyName`, `title`, `author`, `startDate`, `endDate`, `description`, `noOfstudents`, `status`) VALUES('$company', '$title', '$author', '$starttime', '$endtime', '$description', $noOfstudents, 'Taken')";
+            $query2 = "INSERT INTO intershiptags(`companyName`, `title`, `tag`) VALUES('$company', '$title', '$tags')";
+            mysqli_query($connect, $query);
+            mysqli_query($connect, $query2);
+        } else {
+
+            $query = "INSERT INTO internship(`companyName`, `title`, `author`, `startDate`, `endDate`, `description`, `noOfstudents`, `status`) VALUES('$company', '$title', '$author', '$starttime', '$endtime', '$description', $noOfstudents, 'Not Approved')";
+            $query2 = "INSERT INTO intershiptags(`companyName`, `title`, `tag`) VALUES('$company', '$title', '$tags')";
+            mysqli_query($connect, $query);
+            mysqli_query($connect, $query2);
+        }
+
+
+    } elseif ($_POST['radiobutton'] == "Bachelorproject") {
+        echo "Project";
+
+        if($_POST['checkbox'] == "I am student"){
+
+            $query = "INSERT INTO projects(`companyName`, `title`, `author`, `startDate`, `endDate`, `description`, `status`) VALUES('$company', '$title', '$author', '$starttime', '$endtime', '$description', 'Taken')";
+            $query2 = "INSERT INTO projecttags(`companyName`, `title`, `tag`) VALUES('$company', '$title', '$tags')";
+            mysqli_query($connect, $query);
+            mysqli_query($connect, $query2);
+        } else {
+            $query = "INSERT INTO projects(`companyName`, `title`, `author`, `startDate`, `endDate`, `description`, `status`) VALUES('$company', '$title', '$author', '$starttime', '$endtime', '$description', 'Not Approved')";
+            $query2 = "INSERT INTO projecttags(`companyName`, `title`, `tag`) VALUES('$company', '$title', '$tags')";
+            mysqli_query($connect, $query);
+            mysqli_query($connect, $query2);
+        }
+
+    } else {
+        echo "You need to select a suitable study programme.";
+        //echo "<script type='text/javascript'>alert('You need to select a suitable study programme.');</script>";
+    }
+
    
 }
 
